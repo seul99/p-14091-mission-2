86ff25ae751bccbabd9a07c61413a67da75ca74c
sia <piao516@naver.com>
2025-12-11 10:06:46 +0900

    2-02

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<Test> {
