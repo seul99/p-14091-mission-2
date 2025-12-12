4388ef4c376f2e1a7fcbd32242afc675133b136b
sia <piao516@naver.com>
2025-12-12 19:03:31 +0900

    12_12

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,8 +32,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+//    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
